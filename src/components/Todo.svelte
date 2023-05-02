86369db19cbeb62cde5e8bcd45ce7b448483be49
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2023-05-02 18:55:36 +0700

    Update Todo.svelte

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -8,8 +8,10 @@
 </script>
 
 <div class={`border border-gray-500 rounded-md mt-5 m-1 p-2 space-x-3 flex flex-row todo-container items-center ${isDone? "bg-green-300": "bg-inherit"}`}>
-    <input type="checkbox" bind:checked={isDone} 
-    class="w-4 h-4 ml-2" on:change={() => toggleCheck(id)}/>
+    <label>
+        <input type="checkbox" bind:checked={isDone} 
+        class="w-4 h-4 ml-2" on:change={() => toggleCheck(id)}/>
+    </label>
     <p class="font-bold text-lg flex-1">{title}</p>
     <button class={`rounded-md ${isDone? "": "hidden"}`}>
         <Icon.ArchiveBoxXMark size="20" color="red" 
